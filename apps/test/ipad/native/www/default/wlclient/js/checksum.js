73a1c127c1d1f049cc62b6721889410858934608
Hee Woon Siong <woonsiong@cyber-village.net>
2013-08-21 12:04:51 +0800

    first commit

---
+++ apps/test/ipad/native/www/default/wlclient/js/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":2510711544,"date":1377057290525,"machine":"Cybers-MacBook-Pro.local"};
+/* Date: Wed Aug 21 11:54:50 MYT 2013 */
---
+++ apps/test/iphone/native/www/default/wlclient/js/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":1066643411,"date":1377057266944,"machine":"Cybers-MacBook-Pro.local"};
+/* Date: Wed Aug 21 11:54:26 MYT 2013 */
