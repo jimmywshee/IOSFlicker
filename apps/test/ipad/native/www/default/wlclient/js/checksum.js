ac3abf25dfb7380bbf44f66673e4735dcfdbf9ea
Hee Woon Siong <woonsiong@cyber-village.net>
2013-08-21 12:35:38 +0800

    Add project settings

--- apps/test/ipad/native/www/default/wlclient/js/checksum.js
+++ apps/test/ipad/native/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":2510711544,"date":1377057290525,"machine":"Cybers-MacBook-Pro.local"};
-/* Date: Wed Aug 21 11:54:50 MYT 2013 */
+var WL_CHECKSUM = {"checksum":2510711544,"date":1377059659740,"machine":"Cybers-MacBook-Pro.local"};
+/* Date: Wed Aug 21 12:34:19 MYT 2013 */
